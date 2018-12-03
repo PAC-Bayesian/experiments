376af2d384a4af01a20b36f58ed3d7ce1fa410e1
PAC-Bayesian <yyx.inf@outlook.com>
2018-12-03 16:33:00 +0000

    Reset num_samples

--- jobs_1d/job_1d_3.sh
+++ jobs_1d/job_1d_3.sh
@@ -5,6 +5,6 @@ source activate dev-env
  
 # Run the program
 python $exp/params/params_1d/params_3.py
-python $exp/run_low.py ./params/params_1d/params_3 --num_samples 100 --num_sims 10
+python $exp/run_low.py ./params/params_1d/params_3 --num_samples 200 --num_sims 10
 python $exp/run_low.py ./params/params_1d/params_3 --num_samples 1000 --num_sims 10
 python $exp/run_low.py ./params/params_1d/params_3 --num_samples 5000 --num_sims 5
