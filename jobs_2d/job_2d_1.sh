8bb437890ac3da5c2a9d900b2b0fcdc8cfc51924
PAC-Bayesian <yyx.inf@outlook.com>
2018-12-03 04:20:26 +0000

    Modify job_2d_1.sh

--- jobs_2d/job_2d_1.sh
+++ jobs_2d/job_2d_1.sh
@@ -4,7 +4,7 @@ exp=~/experiments
 source activate dev-env
  
 # Run the program
-python $exp/params/params_2d/params_0.py
+python $exp/params/params_2d/params_1.py
 python $exp/run_low.py ./params/params_2d/params_1 --num_samples 200 --num_sims 20
 python $exp/run_low.py ./params/params_2d/params_1 --num_samples 2000 --num_sims 20
 python $exp/run_low.py ./params/params_2d/params_1 --num_samples 20000 --num_sims 10
