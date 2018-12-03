9dc9eda967ce9f1c0772f97a6fbdd347373f57a3
PAC-Bayesian <yyx.inf@outlook.com>
2018-12-03 04:13:28 +0000

    Modify all num_sims to 10

--- jobs_1d/job_1d_1.sh
+++ jobs_1d/job_1d_1.sh
@@ -7,4 +7,4 @@ source activate dev-env
 python $exp/params/params_1d/params_1.py
 python $exp/run_low.py ./params/params_1d/params_1 --num_samples 100 --num_sims 20
 python $exp/run_low.py ./params/params_1d/params_1 --num_samples 1000 --num_sims 20
-python $exp/run_low.py ./params/params_1d/params_1 --num_samples 10000 --num_sims 20
+python $exp/run_low.py ./params/params_1d/params_1 --num_samples 10000 --num_sims 10
