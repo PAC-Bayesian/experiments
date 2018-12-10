c47bc9d8ef4a4432d0b791620a26bf3a1d0509b9
PAC-Bayesian <yyx.inf@outlook.com>
2018-12-10 18:26:59 +0000

    reduce num_samples

--- jobs_add/job_add.sh
+++ jobs_add/job_add.sh
@@ -5,4 +5,4 @@ source activate dev-env
  
 # Run the program
 python $exp/params/params_add/params_add_15_EI.py
-python $exp/run_add.py ./params/params_add/params_add_15_EI --num_samples 1000 --num_sims 1 --seed 111
+python $exp/run_add.py ./params/params_add/params_add_15_EI --num_samples 500 --num_sims 1 --seed 111
